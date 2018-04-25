2109e9c6860ae6eb819c4b50751f911896483524
Aaron <aaron.eberhart@gmail.com>
2018-04-24 21:02:11 -0400

    Fixed header comment

--- Dapylog.py
+++ Dapylog.py
@@ -4,8 +4,9 @@ program and deduce facts from it. The menu
 that runs with any Dapylog instance 
 will let you see the facts and rules the
 program knows, run the reasoner, reset
-the program, add a new fact to the
-database, or quit the program. 
+the program, add a new fact/rule to the
+database, remove a fact/rule, load a 
+new file, or quit the program. 
 
 By default it will use the included 
 file but you can specify your own. 
@@ -19,9 +20,9 @@ bodyOfAnyLength(X,ind)^anyArity(X)->head(Z)
 
 Individuals and predicate names are words.
 Variables are single capital letters.
-Don't use single capital letters for 
-individual names!
-Spaces other than blank lines will 
+Don't use single capital letters or blank
+spaces for individual names!
+Spaces other than blank lines could 
 break the program.
 Datalog comments don't work either. 
 Maybe put them in the python file?
